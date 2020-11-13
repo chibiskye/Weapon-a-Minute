55a77014d2e2ebb4f57c5feebee9ddfdc358b3db
snisonoff <spencer.nisonoff@stonybrook.edu>
2020-11-13 15:30:40 -0500

    Fixed the raycasting for the lazer gun

--- Assets/Scripts/LazerGunScript.cs
+++ Assets/Scripts/LazerGunScript.cs
@@ -7,11 +7,12 @@ public class LazerGunScript : MonoBehaviour
     [SerializeField] private GameObject bullet = null;
     [SerializeField] private Transform bulletParent = null;
     [SerializeField] private Transform shootDirection = null;
-    // [SerializeField] private float range = 3.0f;
+    [SerializeField] private float range = 3.0f;
     [SerializeField] private float fireRate = 1.0f;
     
-    private InputManager inputManager;
+    private InputManager inputManager = null;
     private bool canShoot = true;
+    int layerMask = ~((1 << 8) | (1 << 10)); //shooting does not affect the player or other bullets
     // private float nextTimeToFire = 0f; //currently not used
 
     // Start is called before the first frame update
@@ -21,36 +22,27 @@ public class LazerGunScript : MonoBehaviour
         // weaponControls.Shooter.Shoot.performed += _ => Shoot();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        DebugRaycast();
+
         // Check if player clicked button for shoot
         bool playerShoot = inputManager.GetPlayerAttacked();
         if (playerShoot) Shoot();
     }
 
-    // void FixedUpdate()
-    // {
-    //     int layerMask = 1 << 8; //does not affect the player
-    //     layerMask = ~layerMask;
-
-    //     if (Input.GetButtonDown("Fire1") )//&& Time.time >= nextTimeToFire)
-    //     {
-    //         nextTimeToFire = Time.time + 1f / fireRate;
-    //         Shoot();
-    //     }
-
-    //     RaycastHit hit;
-    //     if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, range, layerMask))
-    //     {
-    //         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * hit.distance, Color.yellow);
-    //         //Debug.Log("Did Hit");
-    //     }
-    //     else
-    //     {
-    //         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 1000, Color.white);
-    //        // Debug.Log("Did not Hit");
-    //     }
-    // }
+    void DebugRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, range, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * hit.distance, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 1000, Color.white);
+        }
+    }
 
     IEnumerator WaitToShoot()
     {
@@ -65,6 +57,16 @@ public class LazerGunScript : MonoBehaviour
 
         // Check if able to shoot
         if (!canShoot) return;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, range, layerMask))
+        {
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.Log("Did not Hit");
+        }
 
         // Instantiate bullet
         GameObject g = Instantiate(bullet, shootDirection.position, shootDirection.rotation, bulletParent);
