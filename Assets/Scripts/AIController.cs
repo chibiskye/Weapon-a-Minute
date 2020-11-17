fbe2c77f463fab6b9ca2454ccbc0fc4638f55db5
chibiskye <mongting.gao@stonybrook.edu>
2020-11-17 03:10:28 -0500

    Redefined some variable types

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -3,100 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-//Current AI Specific Code was given by a tutorial by 'Dave / GameDevelopment' on YouTube
+//Current AI Specific Code was implemented by following a tutorial by 'Dave / GameDevelopment' on YouTube
 public class AIController : MonoBehaviour
 {
-    [SerializeField] private HealthBar healthBar = null;
-    // [SerializeField] private Transform groundTransform = null;
-    // [SerializeField] private float groundDistance = 0.4f;
-    [SerializeField] private int maxHealth = 100;
-    // [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private EnemySwordScript weapon;
+    //Components
+    [SerializeField] private LayerMask whatIsGround, whatIsPlayer;
+    [SerializeField] private Transform player = null;
+    private NavMeshAgent agent = null;
+    private EnemySwordScript weapon = null;
 
-    public NavMeshAgent agent;
-    public Transform player;
-    public LayerMask whatIsGround, whatIsPlayer;
+    //Health
+    // [SerializeField] private HealthBar healthBar = null;
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth = 100;
 
     //Patroling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
+    [SerializeField] private float walkPointRange = 10.0f;
+    [SerializeField] private float walkPointCheckRange = 2.0f;
+    public Vector3 walkPoint; // public for debug purposes
+    private bool walkPointSet = false;
 
     //Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
+    [SerializeField] private float timeBetweenAttacks = 1.0f;
+    private bool alreadyAttacked = false;
 
     //States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private int currentHealth = 100;
+    [SerializeField] private float sightRange = 20f;
+    [SerializeField] private float attackRange = 8f;
+    public bool playerInSightRange = false; // public for debug purposes
+    public bool playerInAttackRange = false; // public for debug purposes
 
     private void Awake()
     {
         player = GameObject.Find("DummyPlayerArmed").transform;
         agent = GetComponent<NavMeshAgent>();
-    }
-
-    private void Patroling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-        {
-            agent.SetDestination(walkPoint);
-        }
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-        {
-            walkPointSet = false;
-        }
-    }
-    private void SearchWalkPoint()
-    {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-        {
-            walkPointSet = true;
-        }
-    }
-    private void ChasePlayer()
-    {
-        agent.SetDestination(player.position);
-    }
-    private void AttackPlayer()
-    {
-        //Make sure enemy doesn't move
-        agent.SetDestination(transform.position);
-
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
-        {
-            weapon.Attack();
-
-
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        }
-    }
-    private void ResetAttack()
-    {
-
-    }
-
-    void Start()
-    {
         currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        // healthBar.SetMaxHealth(maxHealth);
     }
 
     private void FixedUpdate()
@@ -105,19 +47,103 @@ public class AIController : MonoBehaviour
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
+        // Detect and update state
         if (!playerInSightRange && !playerInAttackRange) { Patroling(); }
         if (playerInSightRange && !playerInAttackRange) { ChasePlayer(); }
         if (playerInAttackRange && playerInSightRange) { AttackPlayer(); }
     }
 
-    public void TakeDamage(int damage)
+    // Patrol when player is not in sight
+    private void Patroling()
+    {
+        // Set next random walk point
+        if (!walkPointSet) SearchWalkPoint();
+
+        // Move AI towards walk point
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        // Check if AI reached walk point
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (distanceToWalkPoint.magnitude < 1.0f)
+        {
+            walkPointSet = false; // calculate next walk point
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        // Set walk point to random point
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        // Check if walk point is valid point on map
+        if (Physics.Raycast(walkPoint, -transform.up, walkPointCheckRange, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    // Chase the player when player enters sight range
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    // Attack the player if player spotted and in attack range
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        // Make AI face the player when attacking
+        transform.LookAt(player);
+
+        // Check if AI has already attacked the player
+        if (!alreadyAttacked)
+        {
+            weapon.Attack();
+
+            //According to Unity Docs, Coroutines work better than Invoke, so using Coroutine instead
+            // alreadyAttacked = true;
+            // Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            StartCoroutine(ResetAttack());
+        }
+    }
+
+    // Prevent the AI from attacking too often
+    IEnumerator ResetAttack()
+    {
+        alreadyAttacked = true;
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        alreadyAttacked = false;
+    }
+
+    // TODO: replace with universal health script, if more convenient
+    private void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Below are methods used for debugging
+    // ---------------------------------------------------------------------------------------------
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 
 }
