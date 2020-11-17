2398aeb5f4b2e1e24f9739f48856fa73cb144eae
snisonoff <spencer.nisonoff@stonybrook.edu>
2020-11-17 01:05:53 -0500

    Added code for the AI (Currently needs the NavMeshSurface Script)

---
+++ Assets/Scripts/EnemySwordScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySwordScript : MonoBehaviour
+{
+
+
+    [SerializeField] private float range = 8.0f;
+    private int layerMask = ~(1 << 13); //attacking doesn't affect the enemies
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Attack()
+    {
+        //For now we assume the weapon is holding a sword
+        Vector3 rayOrigin = transform.position;
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, transform.forward, out hit, range, layerMask))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            Debug.Log("missed");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        DebugRaycast();
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Below are methods used for debugging
+    // ---------------------------------------------------------------------------------------------
+
+    void DebugRaycast()
+    {
+        Vector3 rayOrigin = transform.position;
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, transform.forward, out hit, range, layerMask))
+        {
+            Debug.DrawRay(rayOrigin, transform.forward * hit.distance, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(rayOrigin, transform.forward * 1000, Color.white);
+        }
+    }
+}
