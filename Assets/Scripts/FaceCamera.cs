9d9e2ded34087ef43c4cbb12cd9716d07224572a
snisonoff <spencer.nisonoff@stonybrook.edu>
2020-11-23 23:53:46 -0500

    Enemy health bars now face the camera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject camera = null;
+    void FixedUpdate(){
+        transform.LookAt(camera.transform);
+    }
+}
