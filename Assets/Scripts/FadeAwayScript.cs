ff79fb33a53c55d9dafc06caa020585406022127
chibiskye <mongting.gao@stonybrook.edu>
2020-12-05 04:41:24 -0500

    Buttons to title, controls, and high score screens work
    Added a fade away screen

---
+++ Assets/Scripts/FadeAwayScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAwayScript : MonoBehaviour
+{
+    [SerializeField] private float fadeDuration = 2.0f;
+
+    private Image image = null;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        image.canvasRenderer.SetAlpha(0);
+    }
+
+    public IEnumerator FadeAway()
+    {
+        // Fade from transparent to opaque
+        image.CrossFadeAlpha(1, fadeDuration, false);
+
+        // Fade from opaque to transparent
+        image.canvasRenderer.SetAlpha(1);
+        image.CrossFadeAlpha(0, fadeDuration, false);
+        yield return new WaitForSeconds(fadeDuration);
+    }
+}
