d70cdc3ed50f999ba3ca66aba95a7e2ade893aa4
snisonoff <spencer.nisonoff@stonybrook.edu>
2020-12-08 17:46:07 -0500

    Patched an issue where the flying enemy would still be active after being defeated

---
+++ Assets/Scripts/TurnOffPatch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffPatch : MonoBehaviour
+{
+    [SerializeField] GameObject turnOffSignal = null;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!turnOffSignal.active) {
+            gameObject.SetActive(false);
+        }
+    }
+}
