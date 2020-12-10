2d6990f05e542f2d16e1601623b1b3ae0f81e518
chibiskye <mongting.gao@stonybrook.edu>
2020-12-10 05:55:13 -0500

    UI updates for health bar, timer, and weapon displays

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class ScreenScript : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     [SerializeField] private FadeAwayScript fadeAwayScript = null;
     [SerializeField] private GameObject titleScreen = null;
